674875ddc106102173773ee738756cb063b35e70
Ignacio Iglesias <ignacioiglesias8@gmail.com>
2023-08-07 23:18:57 -0300

    comenzando entrega 4

---
+++ entregable4/src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import router from './router/router.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/api', router);
+
+const PORT= 8080;
+app.listen(PORT, (err, res) => {
+    console.log(`servidor en el PORT: ${PORT}`)
+});
