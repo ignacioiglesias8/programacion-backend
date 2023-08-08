1a22a5757c2fee60ad56090ac15ca1f399d450fd
Ignacio Iglesias <ignacioiglesias8@gmail.com>
2023-08-08 00:57:50 -0300

    comenzando con clase 9

--- entregable4/src/app.js
+++ entregable4/src/app.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import router from './router/router.js';
+import handlebars from 'handlebars';
+//import __dirname from './utils.js';
 
 const app = express();
+
+app.engine('handlebars', handlebars.engine());
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
