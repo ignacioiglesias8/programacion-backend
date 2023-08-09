84c3cda71e9bc84bd3a6231f43530de01e805a11
Ignacio Iglesias <ignacioiglesias8@gmail.com>
2023-08-08 23:54:09 -0300

    agregando carpeta public

--- entregable4/src/app.js
+++ entregable4/src/app.js
@@ -21,9 +21,11 @@ app.get('/', (req, res) => {
     );
 });
 
+app.use(express.static(__dirname + '/../public'));
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static('../public'));
+
 
 app.use('/api', router);
 
