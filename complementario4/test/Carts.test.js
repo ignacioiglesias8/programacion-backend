288e3c01c886662afa64e07f442fbb760d9758bc
Ignacio Iglesias <ignacioiglesias8@gmail.com>
2023-12-07 10:55:03 -0300

    test products

---
+++ complementario4/test/Carts.test.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import chai from 'chai';
+import supertest from 'supertest';
+
+mongoose.connect('mongodb+srv://ignacioiglesias8:9HzbVxanl92pkney@cluster0.paoiaa9.mongodb.net/ecommerce?retryWrites=true&w=majority')
+
+const expect = chai.expect;
+const request = supertest('http://localhost:8080/api/carts')
+
+describe('Testing Carts Router', () => {
+
+});
---
+++ complementario4/test/Products.test.js
@@ -0,0 +1,117 @@
+import mongoose from "mongoose";
+import chai from 'chai';
+import supertest from 'supertest';
+
+mongoose.connect('mongodb+srv://ignacioiglesias8:9HzbVxanl92pkney@cluster0.paoiaa9.mongodb.net/ecommerce?retryWrites=true&w=majority')
+
+const expect = chai.expect;
+const request = supertest('http://localhost:8080/api/products')
+let createdProductId;
+
+describe('Testing Products Router', () => {
+    describe('GET /', () => {
+        it('Debe retornar un arreglo de productos', async () => {
+
+            const {
+                statusCode, 
+                _body
+            } = await request.get('/');
+
+            expect(statusCode).to.equal(200);
+            expect(_body.payload).to.be.an('array');
+        });
+    });
+
+    describe('GET /:pid', () => {
+        it('Debe retornar un producto con Id válido', async () => {
+            const productId = '654e8d9cdd7bc5ac119b66c0';
+
+            const {
+                statusCode, 
+                _body
+            } = await request.get(`/${productId}`);
+
+            expect(statusCode).to.equal(200);
+            expect(_body).to.have.property('product');
+        });
+
+        it('No debe retornar un producto: Id inválido', async () => {
+            const productId = '654e8d9cdd7bc5ac119b66c2';
+
+            const response = await request.get(`/${productId}`);
+
+            expect(response.statusCode).to.equal(200);
+            expect(response.res._events).to.have.property('error');
+        });
+    });
+
+    describe('POST /', () => {
+        it('Debe crear un producto nuevo con Id de Mongo', async () => {
+            const productMock = {
+                title: 'Producto Test',
+                description: 'Esto es un producto test',
+                price: 19.99,
+                code: 'TP001',
+                stock: 10,
+                category: 'Categoría test',
+                owner: 'admin@test.com',  
+            };
+
+            const {
+                statusCode, 
+                _body
+            } = await request.post('/').send(productMock);
+
+            createdProductId = _body.payload._id;
+
+            expect(statusCode).to.equal(200);
+            expect(_body).to.have.property('payload').that.has.property('_id');
+        });
+
+        it('No debe crear un nuevo producto: error del servidor', async () => {
+            const productMock = {
+                title: 'Producto Test',
+                description: 'Esto es un producto test',
+                price: 19.99,
+                code: 'pack01',
+                stock: 10,
+                category: 'Categoría test',
+                owner: 'admin@test.com',  
+            };
+
+            const response = await request.post('/').send(productMock);
+
+            expect(response.status).to.equal(500);
+            expect(response.res._events).to.have.property('error');
+        });
+    });
+
+    describe('PUT /:pid', () => {
+        it('Debe modificar el producto creado', async () => {
+            const productId = createdProductId;
+            const modifications = {status: "false"};
+
+            const {
+                statusCode, 
+                _body
+            } = await request.put(`/${productId}`).send(modifications);
+
+            expect(statusCode).to.equal(200);
+            expect(_body.updatedProduct[0]).to.have.property('status').that.equals(false);
+        });
+    });
+
+    describe('DELETE /:pid', () => {
+        it('Debe eliminar el producto creado y modificado', async () => {
+            const productId = createdProductId;
+
+            const {
+                statusCode, 
+                _body
+            } = await request.delete(`/${productId}`)
+
+            expect(statusCode).to.equal(200);
+            expect(_body.result).to.have.property('acknowledged').that.equals(true);
+        });
+    });
+});
