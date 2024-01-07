e2fcbc5edc92ddc9d02140473e7d7b50546c2c0f
Ignacio Iglesias <ignacioiglesias8@gmail.com>
2024-01-07 02:38:16 -0300

    mejoras en vista de gestor de productos

--- proyectofinal/public/js/createProductButton.js
+++ proyectofinal/public/js/createProductButton.js
@@ -1,54 +1,47 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let updateProductButton = document.querySelector(".updateProduct");
-    if (updateProductButton) {
-        updateProductButton.addEventListener("click", function () {
-            const productId = document.getElementById("productId").value;
+    let createProductButton = document.querySelector(".createProduct");
+    if (createProductButton) {
+        createProductButton.addEventListener("click", function () {
             const title = document.getElementById("title").value;
             const description = document.getElementById("description").value;
             const price = document.getElementById("price").value;
             const code = document.getElementById("code").value;
             const stock = document.getElementById("stock").value;
             const category = document.getElementById("category").value;
-            const status = document.getElementById("status").value;
+            const owner = document.getElementById("owner").value;
 
-        if (!productId) {
-            alert("Por favor, ingrese un ID de producto válido.");
+        if (!confirm("¿Está seguro de que desea crear este producto?")) {
             return;
         }
 
-        if (!confirm("¿Está seguro de que desea modificar este producto?")) {
-            return;
-        }
-
-        const modifications = {
+        const product = {
             title,
             description,
             price,
             code,
             stock,
             category,
-            status,
+            owner,
         };
 
-        fetch(`/api/products/${productId}`, {
-            method: "PUT",
+        fetch(`/api/products/`, {
+            method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(modifications),
+            body: JSON.stringify(product),
         })
         .then((response) => {
             if (response.ok) {
-                alert("Producto modificado correctamente");
-                document.getElementById("productId").value = "";
+                alert("Producto creado correctamente");
                 document.getElementById("title").value = "";
                 document.getElementById("description").value = "";
                 document.getElementById("price").value = "";
                 document.getElementById("code").value = "";
                 document.getElementById("stock").value = "";
             } else {
-                alert("No estás autorizado a modificar este producto.")
-                console.error("Error al modificar el producto.");
+                alert("Faltan datos obligatorios o son duplicados.")
+                console.error("Error al crear el producto.");
             }
         })
         .catch((error) => {
